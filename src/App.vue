94ada9b403a649931b1dd32f6efa20138cabea4c
divyank22 <divyankpandey37@gmail.com>
2024-05-31 03:16:15 +0530

    logic of Random quote generator
    using math in JavaScript and defining object in quote

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Randomqoutegenerator from './components/Randomqoutegenerator.vue'
+</script>
+
+<template>
+<Randomqoutegenerator/>
+</template>
+
+
---
+++ src/components/Randomqoutegenerator.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const quotes = ref([
+  {
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    meaning:
+      "This quote by Steve Jobs emphasizes the importance of passion in achieving great results. When you love what you do, it's easier to put in the effort required to excel.",
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+    meaning:
+      "This quote by Theodore Roosevelt suggests that self-belief is a crucial part of achieving any goal. If you believe in your abilities, you've already overcome half the challenge.",
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+    meaning:
+      "This quote by John Lennon is a reminder that sometimes, the best things in life happen unexpectedly. It's important to stay open and flexible to life's surprises.",
+  },
+  {
+    text: "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+    meaning:
+      "This quote by Franklin D. Roosevelt suggests that our doubts and fears can limit our potential. By overcoming these doubts, we can achieve our full potential.",
+  },
+  {
+    text: "Go confidently in the direction of your dreams! Live the life you've imagined.",
+    author: "Henry David Thoreau",
+    meaning:
+      "This quote by Henry David Thoreau encourages us to have confidence in our dreams and to actively work towards them. It's a call to live the life we've imagined, rather than settling for less.",
+  },
+  {
+    text: "The journey of a thousand miles begins with a single step.",
+    author: "Lao Tzu",
+    meaning:
+      "This quote emphasizes that every long journey begins with a first step, no matter how small. It encourages taking the first step towards achieving a large or difficult goal.",
+  },
+]);
+
+const currentquote = ref({ text: "", author: "",meaning:"" });
+
+const getRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.value.length);
+  currentquote.value = quotes.value[randomIndex];
+};
+onMounted(getRandomQuote);
+</script>
+<template>
+  <button @click="getRandomQuote">random</button>
+  <h1>{{ currentquote.text }}</h1>
+  <h1>{{ currentquote.author }}</h1>
+  <h1>{{ currentquote.meaning }}</h1>
+</template>
