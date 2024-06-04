3a5cf5bfaea3ed0fa99d4510298a4ee840b8035d
divyank22 <divyankpandey37@gmail.com>
2024-06-04 06:51:00 +0530

    added style
    center div added boarder

--- src/components/Randomqoutegenerator.vue
+++ src/components/Randomqoutegenerator.vue
@@ -1,12 +1,20 @@
 <script setup>
 import { onMounted } from "vue";
-import useQuotes from '../composables/random_qoute_generator.js'
+import useQuotes from "../composables/random_qoute_generator.js";
 const { currentquote, getRandomQuote } = useQuotes();
-onMounted(currentquote)
+onMounted(currentquote);
 </script>
 <template>
-  <button @click="getRandomQuote">random</button>
-  <h1>{{ currentquote.text }}</h1>
-  <h1>{{ currentquote.author }}</h1>
-  <h1>{{ currentquote.meaning }}</h1>
+  <div class="eventtriggerd">
+    <h1>
+      random
+      <button @click.prevent="getRandomQuote">dose</button>
+      of motivation
+    </h1>
+  </div>
+  <div class="container">
+    <h1>quote: {{ currentquote.text }}</h1>
+    <h1>giver:{{ currentquote.author }}</h1>
+    <h1>what it mean:{{ currentquote.meaning }}</h1>
+  </div>
 </template>
